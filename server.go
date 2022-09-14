c72b22d51bb05758ff22809953271df7c76b1e29
mikha <ya.mikushoff@gmail.com>
2022-09-14 17:46:45 +0300

    refactor: moved to repository root

--- server.go
+++ server.go
@@ -2,37 +2,40 @@ package server
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	log "github.com/sirupsen/logrus"
 )
 
-type Server struct {
+type HttpServer struct {
+	Logger log.Logger
 }
 
-func (s *Server) StartServer(serverUrl string, handler http.Handler) *http.Server {
-	srv := &http.Server{Addr: serverUrl, Handler: handler}
-	log.Error(srv.ListenAndServe())
+func (s *HttpServer) StartServer(port string, handler http.Handler) *http.Server {
+	srv := &http.Server{Addr: ":" + port, Handler: handler}
+	s.Logger.Error(srv.ListenAndServe())
 	return srv
 }
 
-func (s *Server) GetKillSignalChan() chan os.Signal {
+func (s *HttpServer) WaitForKillSignal() {
+	killSignalChan := s.getKillSignalChan()
+	killSignal := <-killSignalChan
+	switch killSignal {
+	case os.Interrupt:
+		s.Logger.Info("got SIGINT...")
+	case syscall.SIGTERM:
+		s.Logger.Info("got SIGTERM...")
+	}
+}
+
+func (s *HttpServer) getKillSignalChan() chan os.Signal {
 	osKillSignalChan := make(chan os.Signal, 1)
 	signal.Notify(osKillSignalChan, os.Interrupt, syscall.SIGTERM)
 
 	return osKillSignalChan
-}
-
-func (s *Server) WaitForKillSignal(killSignalChan <-chan os.Signal) {
-	killSignal := <-killSignalChan
-	switch killSignal {
-	case os.Interrupt:
-		log.Info("got SIGINT...")
-	case syscall.SIGTERM:
-		log.Info("got SIGTERM...")
-	}
 }
 
 func HealthCheckHandler(w http.ResponseWriter, _ *http.Request) {
